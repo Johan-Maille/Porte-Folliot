6521ba9519a7a14d1ab28b39df65137bdef159f1
Johan <johan73460@outlook.fr>
2018-10-16 11:16:10 +0200

    Mise a jour des fichiers

---
+++ js/map.js
@@ -0,0 +1,44 @@
+var map_wrapper = 'map_container';
+var longitude = 45.186606;
+var latitude = 5.763819;
+var bubble_content =	"<p class='map_contacts'>" +
+						"<span>Address : </span>" +
+						"25 rue des glairons, saint martin d'hères 38400" +
+						"</p>";
+
+
+
+
+
+
+function initialize() {
+	var mapOptions = {
+		zoom: 13,
+		center: new google.maps.LatLng(longitude, latitude),
+		mapTypeId: google.maps.MapTypeId.ROADMAP,
+		mapTypeControl: false,
+		streetViewControl:false,
+		scrollwheel : false,
+		zoomControlOptions: {
+	      style: google.maps.ZoomControlStyle.SMALL
+	    }
+	};
+
+	var map = new google.maps.Map(document.getElementById(map_wrapper),mapOptions);
+
+	var marker = new google.maps.Marker({
+	  position:  new google.maps.LatLng(longitude, latitude),
+	  map: map,
+	  icon     : "img/marker.png"
+	});
+
+	var infowindow = new google.maps.InfoWindow({
+			content: bubble_content
+		});
+
+	google.maps.event.addListener(marker, 'click', function() {
+	  infowindow.open(map,marker);
+	});
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
